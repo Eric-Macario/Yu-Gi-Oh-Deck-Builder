57ff9d9be0cfceb529d9fce7f3d8c26eb366434f
Eric <macario41@gmail.com>
2023-07-06 18:42:38 -0400

    routes

--- Backend/models.py
+++ Backend/models.py
@@ -1,26 +1,31 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
 deck_cards = db.Table(
     'deck_cards',
-    db.Column('deck_id', db.Integer, db.ForeignKey('decks.id'), primary_key=True),
-    db.Column('card_id', db.Integer, db.ForeignKey('cards.id'), primary_key=True)
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('deck_id', db.Integer, db.ForeignKey('decks.id')),
+    db.Column('card_id', db.Integer, db.ForeignKey('cards.id'))
 )
+
 
 collection_cards = db.Table(
     'collection_cards',
+    db.Column('id', db.Integer, primary_key=True),
     db.Column('collection_id', db.Integer, db.ForeignKey('collections.id'), primary_key=True),
     db.Column('card_id', db.Integer, db.ForeignKey('cards.id'), primary_key=True)
 )
 
 wishlist_cards = db.Table(
     'wishlist_cards',
+    db.Column('id', db.Integer, primary_key=True),
     db.Column('wishlist_id', db.Integer, db.ForeignKey('wishlists.id'), primary_key=True),
     db.Column('card_id', db.Integer, db.ForeignKey('cards.id'), primary_key=True)
 )
 
-class User(db.Model):
+class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
@@ -32,7 +37,9 @@ class User(db.Model):
     wishlist = db.relationship('Wishlist', back_populates='user')
     decks = db.relationship('Deck', back_populates='user')
 
-class Card(db.Model):
+    serialize_rules = ('-collections.user', '-wishlist.user', '-decks.user')
+
+class Card(db.Model, SerializerMixin):
     __tablename__ = 'cards'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -45,23 +52,24 @@ class Card(db.Model):
     wishlists = db.relationship('Wishlist', secondary=wishlist_cards, back_populates='cards')
     decks = db.relationship('Deck', secondary=deck_cards, back_populates='cards')
 
-class Collection(db.Model):
+    serialize_rules = ('-collections.cards', '-wishlists.cards', '-decks.cards')
+
+class Collection(db.Model, SerializerMixin):
     __tablename__ = 'collections'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     cards = db.relationship('Card', secondary=collection_cards, back_populates='collections')
     user = db.relationship('User', back_populates='collections')
 
-class Deck(db.Model):
+class Deck(db.Model, SerializerMixin):
     __tablename__ = 'decks'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     cards = db.relationship('Card', secondary=deck_cards, back_populates='decks')
-    card = db.relationship('Card', back_populates='decks')
     user = db.relationship('User', back_populates='decks')
 
-class Wishlist(db.Model):
+class Wishlist(db.Model, SerializerMixin):
     __tablename__ = 'wishlists'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
